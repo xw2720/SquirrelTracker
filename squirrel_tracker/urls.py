ec299ca855e58dbfb9b182739c3f3eace2e2dee8
xw2720 <xw2720@columbia.edu>
2021-04-10 23:39:08 +0800

    Merge some codes from Wendy's original seperated project and fix bugs

--- squirrel_tracker/urls.py
+++ squirrel_tracker/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
+"""squirrel_tracker_site URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path  # , include
 from squirrel_tracker.views import MapView
 
-app_name = 'map'
 urlpatterns = [
-    path('', MapView.as_view(), name='map'),
+    path('admin/', admin.site.urls),
+    path('map/', MapView.as_view(), name='map'),
+    # TODO add sightings methods view paths here
 ]
--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -2,19 +2,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from squirrel_tracker.models import Squirrel
+
 
 def index(request):
-    pass
-    # return render(request, 'sightings/index.html', context)
+    return redirect('map/')
 
 
 # Create your views here.
 
 class MapView(TemplateView):
+    """
+    Merge from Wendy's original seperated project
+    ref: https://docs.djangoproject.com/zh-hans/3.1/topics/class-based-views/
+    """
     template_name = "map/map.html"
 
-    def get(self, request):
-        return render(request, self.template_name)
-
-    def post(self, request):
-        return render(request, self.template_name)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # TODO add query of squirrel data here
+        sightings = Squirrel.objects.all()
+        context["sightings"] = sightings
+        return context
