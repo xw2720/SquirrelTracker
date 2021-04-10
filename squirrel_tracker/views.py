27abfa00e0a093fc352dcea3f23fe8afd0c09d68
little22ww <yh3309@columbia.edu>
2021-04-10 12:55:37 +0800

    Draw a map

--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 def index(request):
     pass
     # return render(request, 'sightings/index.html', context)
+
+
+# Create your views here.
+
+class MapView(TemplateView):
+    template_name = "map/map.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        return render(request, self.template_name)
